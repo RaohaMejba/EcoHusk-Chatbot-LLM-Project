8da2644c187f7415e2b514118d603c4b61ad3b3f
MEZBA RAOHA BIN <raoha_50@MacBookPro.lan>
2025-05-19 03:46:15 +0800

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EcoHusk Chatbot',
+    version= '0.0.0',
+    author= 'Raoha Bin Mejba + SL24225002',
+    author_email= 'raoha@mail.ustc.edu.cn',
+    packages= find_packages(),
+    install_requires = []
+
+)
